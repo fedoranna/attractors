74fc37ba3d96d1a7e515aff17fc2c43e8906c657
matevarga <m@matevarga.net>
2015-05-20 20:40:26 +0200

    Missed out InitializeAttractor #13

--- InitializeAttractor.m
+++ InitializeAttractor.m
@@ -80,66 +80,7 @@ else
 end
 
 % Delete weights 
-switch P.weight_deletion_mode
-    
-    case 'probabilistic'
-        
-        nbof_eliminated = round(  numel(W.state) * (1-P.connection_density)   );
-        W.eliminated = randperm(numel(W.state), nbof_eliminated);
-        W.eliminated = sort([W.eliminated, diagonal]);
-        
-    case 'exact'        
-        
-        P.connections_per_neuron = P.connection_density * P.nbof_neurons;
-        P.loading = P.nbof_patterns/P.connections_per_neuron;
-        
-        c = P.connections_per_neuron;
-        N = size(W.state,1);
-        dummy = ones(size(W.state));
-        
-        dummy(diagonal) = 0;
-        for i = 1:N
-            avoid = [];
-            all = sum(dummy, 1);
-            current = all(i);
-            todelete = current - c;
-            if todelete < 0
-                todelete = 0;
-            end
-            
-            choosefrom = find(dummy(:,i)); % the index of nonzero elements
-            
-            % Avoiding those neurons that already have c weights
-            for j = 1:numel(all)
-                if all(j) <= c
-                    avoid = [avoid, j]; % the index of neurons that already have c weights
-                end
-            end
-            if isempty(avoid) == 0
-                for j = numel(avoid) :-1 : 1
-                    [row, column, v] = find(choosefrom==avoid(j));
-                    choosefrom(row) = [];
-                end
-            end
-            
-            % Choosing weights to delete
-            if todelete > numel(choosefrom)
-                deleted = choosefrom;
-            else
-                indices = randperm(numel(choosefrom), todelete);
-                deleted = choosefrom(indices);
-            end
-            dummy(deleted,i) = 0;
-            dummy(i, deleted) = 0;
-        end
-        
-        % Listing weights to delete
-        not_eliminated = find(dummy);
-        eliminated = 1:numel(W.state);
-        eliminated(not_eliminated) = [];
-        W.eliminated = eliminated;
-                  
-end
+W.eliminated = P.weight_deletion_mode(W, P, diagonal);
 
 W.state(W.eliminated) = 0;
 
